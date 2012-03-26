12edba2bd8ad921540a38f768db711c7efa98182
Nicolas <nicolas.fouche@viacesi.fr>
2012-03-26 20:02:29 +0200

    changement sur dashboard

--- content/commande.php
+++ content/commande.php
@@ -1,5 +1,19 @@
 <section id="content_ppal">
+<div id="commade">
+    <div class="logo"></div>
+    <fieldset>Bon de commande</fieldset>
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</div>    
     
     
     
