c8fd50faa18b8de483d19dd9c8d934552bbebfdd
Nicolas <nicolas.fouche@viacesi.fr>
2012-03-27 00:01:28 +0200

    changement sur dashboard

--- content/commande.php
+++ content/commande.php
@@ -1,7 +1,16 @@
 <section id="content_ppal">
 <div id="commade">
     <div class="logo"></div>
-    <fieldset>Bon de commande</fieldset>
+   <FORM method=post action="..." >
+<FIELDSET>
+<LEGEND align=top>Bon de Commandes </LEGEND><br>
+<div style="float:left">
+<p>&nbsp;Num&eacute;ro de commande :<?php  echo"0023455A"   ?>&nbsp;</div><div style="float:right;margin-right:10px">
+Date : <?php echo $jours[date("w")] . " " . date("d") . " " . $mois[date("m")-1] . " " . date("Y"); ?></div></p>
+
+</FIELDSET>
+</FORM>
+
     
     
     
