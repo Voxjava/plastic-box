f23d7addecc8f8160756b873bbe76f52a34359d0
Nicolas <nicolas.fouche@viacesi.fr>
2012-03-26 19:26:53 +0200

    changement sur dashboard

--- content/dashboard.php
+++ content/dashboard.php
@@ -95,7 +95,17 @@
                 ultricies at at cras tempus.</p>       
              </div>    
             </div>
-            
+            <div class="group">
+            <li><a href="#">ferfrefe</a></li>  
+            <div>
+            <p>Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. </p>
+			<ul>
+				<li>List item one</li>
+				<li>List item two</li>
+				<li>List item three</li>
+			</ul>
+             </div>    
+            </div>
         </ul>        
         </div> 
     </div>  
