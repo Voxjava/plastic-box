a3a4df8c6d62970e51c1858d3afb7926b5cd3364
Nicolas <nicolas.fouche@viacesi.fr>
2012-03-27 21:25:11 +0200

    changement sur dashboard

--- content/commande.php
+++ content/commande.php
@@ -1,44 +1,116 @@
 <section id="content_ppal">
-<div id="commade">
-    <div class="logo"></div>
-   <FORM method=post action="..." >
-<FIELDSET>
-<LEGEND align=top>Bon de Commandes </LEGEND><br>
-<div style="float:left">
-<p>&nbsp;Num&eacute;ro de commande :<?php  echo"0023455A"   ?>&nbsp;</div><div style="float:right;margin-right:10px">
-Date : <?php echo $jours[date("w")] . " " . date("d") . " " . $mois[date("m")-1] . " " . date("Y"); ?></div></p>
+    <div id="commade">
+        <div class="logo"></div>
+        <FORM method=post action="" >
+            <FIELDSET style="width:562px">
+                <LEGEND align=top>Bon de Commandes </LEGEND><br>
+                <div style="float:left">
+                    <label>&nbsp;Num&eacute;ro de commande : <?php echo"0023455A" ?>&nbsp;</label>
+                </div>
+                <div style="float:right;margin-right:10px">
+                    <label>Date : <?php echo $jours[date("w")] . " " . date("d") . " " . $mois[date("m") - 1] . " " . date("Y"); ?></label>
+                </div>
+                <div class="clear"></div>
+            </FIELDSET>
+            <FIELDSET style="width:762px;margin-top:20px;">
+                <legend>Indications et informations particuliéres</legend>
+                
+<form name="f1">
+<table id="Commande_table">
+    <tr>
+        <td>Article</td>
+        <td>Quantité commandé</td>
+        <td>Quantité en stock</td>
+        <td>Production</td>
+    </tr>
+    <tr>
+    <td>
+        <select>
+        <option></option>
+        <option>Article 1</option>
+        <option>Article 2</option>
+        <option>Article 3</option>
+        </select>
+    </td>
+    <td><input type="text" size="6" name="c1unite" placeholder="0,00" /></td>
+    <td>Stock article </td>
+    <td> <input type="text" name="c1prix" size="6" value="0.00" /></td>
+    </tr>
+    <tr>
+        <td>
+        <select>
+        <option></option>
+        <option>Article 1</option>
+        <option>Article 2</option>
+        <option>Article 3</option>
+        </select>
+    </td>
+        <td><input type="text" size="6" name="c2" placeholder="0,00"  /></td>
+        <td>stock Article</td>
+        <td><input type="text" name="c2prix" size="6" value="0.00" /></td>
+    </tr>
+    <tr>
+        <td>
+        <select>
+        <option></option>
+        <option>Article 1</option>
+        <option>Article 2</option>
+        <option>Article 3</option>
+        </select>
+    </td>
+        <td> <input type="text" size="6" name="c3" placeholder="0,00" /></td>
+        <td>Stock article</td>
+        <td> <input type="text" name="c3prix" size="6" value="0.00" /></td>
+    </tr>
+    <tr>
+        <td>Frais de port</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td><input type="text" name="port" value="6.95" size="6" /></td>
+    </tr>
+    <tr>
+        <td>TVA</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td><input type="text" name="tva" value="0.00" size="6" /> </td>
+    </tr>
+    <tr>
+        <td>TOTAL</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td> <input type="text" name="total" value="0.00" size="6" /></td>
+    </tr>
+</table>
+</p>
+</form>
 
-</FIELDSET>
-</FORM>
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-</div>    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+    </div>    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </section>
